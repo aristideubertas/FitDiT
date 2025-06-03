08b9df26e3506e4d0c11010850ce26b83570ec89
Aristide Fumagalli <aristide.fumagalli@ubertasconsulting.com>
2025-06-03 15:01:09 +0100

    memory offload

--- train_stage1.sh
+++ train_stage1.sh
@@ -1,2 +1,3 @@
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 CUDA_VISIBLE_DEVICES="0" \
 accelerate launch --config_file configs/deepspeed_config.yaml --num_processes 1 --main_process_port 10080 train_fitdit_stage1.py --config configs/train.yaml  # > log_stage1.txt 2>&1 & 
